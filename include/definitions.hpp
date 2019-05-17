3b48c9bf6a3b975607a55c6f3df1086df72ef3f4
Masa <masaradio78@gmail.com>
2019-05-17 18:35:32 +0200

    With new file

---
+++ include/definitions.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+// CONFIGURATION //
+// Fenêtre
+#define WINDOW_WIDTH 500
+#define WINDOW_HEIGHT 500
+
+// Variables de taille de la classe Object
+#define NUMBER_OBJECTS 15
+#define NUMBER_COLORS 15
+
+// Objects
+#define TETE 0
+#define CORPS 1
+#define BRAS 2
+#define AVANT_BRAS 3
+#define CANON 4
+#define JAMBE 5
+#define AVANT_JAMBE 6
+#define PIED 7
+#define SECTION 8
+#define BULLET 9
+
+// Colors
+#define BLUE 0
+#define WHITE 1
+#define RED 2
+
+// Megaman
+#define VITESSE 0.5
+#define PUISSANCE_BULLET 0.1
+#define PUISSANCE_MAX_BULLET 2
+#define TIME_SHOOT 1
+
+
+// CAN'T TOUCH THIS //
+// Formes
+#define GL_CERCLE 0x0010
+#define GL_TRAPEZE 0x0011
+#define GL_TRAPEZE3D 0x0021
+#define GL_POLYGON3D 0x0022
